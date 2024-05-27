25931539b52c9d5aad3725323778e571ea921d49
Ifrah khan <74492755+ifrah447@users.noreply.github.com>
2024-05-28 02:11:15 +0500

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,10 @@
+
+print("school management system")
+print("students bio-managment")
+student_1=["name=Haris","father's name=Anwar","age=13","class=6","roll_no=72"]
+student_2=["name=Maria","father's name=Abbas","age=12","class=6","roll_no=23"]
+student_3=["name=Hellen","father's name=Jack","age=11","class=6","roll_no=26"]
+student_name=str(input("enter student's name: ")
+if student_name == "Haris" :
+    print("student details are: ",student_1[0:4])
+
